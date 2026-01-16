e15166e302ac2309964c8fc087d7cad785017363
kostya <kostyanuda@gmail.com>
2026-01-16 12:24:21 +0500

    проверить в техникуме

--- httpsnewpat/Program.cs
+++ httpsnewpat/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +15,64 @@ namespace httpsnewpat
 	{
 		static void Main(string[] args)
 		{
+			WebRequest request = WebRequest.Create("http://www.permaviat.ru/main");
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			Console.WriteLine(response.StatusDescription);
+			Stream dataStream = response.GetResponseStream();
+			StreamReader reader = new StreamReader(dataStream);
+			string responseFromServer = reader.ReadToEnd();
+			Console.WriteLine(responseFromServer);
+			reader.Close();
+			dataStream.Close();
+			response.Close();
+			Console.Read();
 		}
+		public static void SingIn(string Login, string Password)
+		{
+			string url = "http://news.permaviat.ru/ajax/login.php";
+			Debug.WriteLine($"Выполнен запрос: {url}");
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method = "POST";
+			request.ContentType = "application/x-www-form-urlencoded";
+			request.CookieContainer = new CookieContainer();
+			string postData = $"login={Login}&password={Password}";
+			byte[] Data = Encoding.ASCII.GetBytes(postData);
+			request.ContentLength = Data.Length;
+			using (var stream = request.GetRequestStream())
+			{
+				stream.Write(Data, 0, Data.Length);
+			}
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+			string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+			Console.WriteLine(responseFromServer);
+		}
+		public static void GetContent(Cookie Token)
+		{
+			string url = "http://news.permaviat.ru/main";
+			Debug.WriteLine($"Выполняем запрос: {url}");
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.CookieContainer = new CookieContainer();
+			request.CookieContainer.Add(Token);
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+			string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+			Console.WriteLine(responseFromServer);
+		}
+		public static void ParsineHtml(string htmlCode)
+		{
+			var html = new HtmlDocument();
+			html.LoadHtml(htmlCode);
+			var Document = html.DocumentNode;
+			IEnumerable DivsNews = Document.Descendants(0).Where(n => n.HasClass("news"));
+			foreach (HtmlNode DivNews in DivsNews)
+			{
+				var src = DivNews.ChildNodes[1].GetAttributeValue("src", "none");
+				var name = DivNews.ChildNodes[3].InnerText;
+				var description = DivNews.ChildNodes[5].InnerText;
+				Console.WriteLine(name + "\n" + "Изображение: " + src + "\n" + "Описание: " + description + "\n");
+			}
+		}
+
 	}
 }
